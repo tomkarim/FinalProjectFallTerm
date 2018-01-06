ac0c6ba74417f6e1c96a45049695bf5587b239d7
Tanveer <tmahmood@stuy.edu>
2018-01-06 11:51:03 -0500

    Added background, made background fullscreen

--- MarioBros/core/src/com/tbros/supermariobros/MarioBros.java
+++ MarioBros/core/src/com/tbros/supermariobros/MarioBros.java
@@ -1,5 +1,7 @@
 package com.tbros.supermariobros;
 
+import java.util.ArrayList;
+import java.util.Random;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,26 +10,31 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MarioBros extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+	ArrayList<Texture> goompas;
+	Random spawnRate;
+
 	@Override
 	public void create () {
+		background = new Texture("background.png");
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background.setWrap(Texture.TextureWrap.ClampToEdge, Texture.TextureWrap.ClampToEdge);
+		goompas = new ArrayList<Texture>();
+		spawnRate = new Random();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		background.dispose();
 	}
 }
